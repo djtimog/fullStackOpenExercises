b0acdccd2260d3877c779f7b68c41ce3a92432e4
Christian Ogunleye <ogunleyetimilehin15@gmail.com>
2026-01-04 13:27:31 +0100

    Data for countries, step 2 exercise done

--- part2/countryData/src/component.jsx
+++ part2/countryData/src/component.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export const CountryBox = ({ countriesToShow }) => {
   if (!countriesToShow || countriesToShow.length === 0) return null;
   if (countriesToShow.length > 10) {
@@ -6,29 +8,46 @@ export const CountryBox = ({ countriesToShow }) => {
     return (
       <div>
         {countriesToShow.map((country) => (
-          <p key={country.area}>{country.name.common}</p>
+          <CountryList country={country} key={country.area} />
         ))}
       </div>
     );
   } else {
     const country = countriesToShow[0];
-    const languages = Object.values(country.languages);
-    return (
-      <div>
-        <h1>{country.name.common}</h1>
-
-        <p>Capital {country.capital}</p>
-        <p>Area {country.area}</p>
-
-        <h3>Languages</h3>
-        <ul>
-          {languages.map((lang) => (
-            <li key={lang}>{lang}</li>
-          ))}
-        </ul>
-
-        <img src={country.flags.png} alt={`${country.name.common} flag`} />
-      </div>
-    );
+    return <CountryView country={country} />;
   }
+};
+const CountryList = ({ country }) => {
+  const [showCountry, setShowCountry] = useState(false);
+  return (
+    <div>
+      {country.name.common}
+      <button onClick={() => setShowCountry(!showCountry)}>
+        {!showCountry ? "Show" : "Hide"}
+      </button>
+      {showCountry && <CountryView country={country} />}
+    </div>
+  );
+};
+
+const CountryView = ({ country }) => {
+  const languages = Object.values(country.languages);
+
+  return (
+    <div>
+      <h1>{country.name.common}</h1>
+
+      <p>Capital {country.capital}</p>
+      <p>Area {country.area}</p>
+
+      <h3>Languages</h3>
+      <ul>
+        {languages.map((lang) => (
+          <li key={lang}>{lang}</li>
+        ))}
+      </ul>
+
+      <img src={country.flags.png} alt={`${country.name.common} flag`} />
+    </div>
+  );
 };
