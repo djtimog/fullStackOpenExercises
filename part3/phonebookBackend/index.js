8fb04a33334ff562f875663614242ae4526a6a2f
Christian Ogunleye <ogunleyetimilehin15@gmail.com>
2026-01-04 23:23:04 +0100

    exercise 3.7: Phonebook backend step 7 Done

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -23,9 +23,11 @@ let persons = [
 const PORT = 3001;
 
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 app.use(express.json());
+app.use(morgan("tiny"));
 
 app.get("/api/persons", (req, res) => {
   res.json(persons);
