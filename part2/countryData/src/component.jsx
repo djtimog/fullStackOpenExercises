393e0e2fd5e6377e86f60fd981345a1ec63392d8
Christian Ogunleye <ogunleyetimilehin15@gmail.com>
2026-01-04 13:13:39 +0100

    initailized project and Data for countries, step 1 exercise done

---
+++ part2/countryData/src/component.jsx
@@ -0,0 +1,34 @@
+const CountryBox = ({ countriesToShow }) => {
+  if (!countriesToShow || countriesToShow.length === 0) return null;
+  if (countriesToShow.length > 10) {
+    return <p>Too many matches, specify another filter</p>;
+  } else if (countriesToShow.length > 2) {
+    return (
+      <div>
+        {countriesToShow.map((country) => (
+          <p key={country.area}>{country.name.common}</p>
+        ))}
+      </div>
+    );
+  } else {
+    const country = countriesToShow[0];
+    const languages = Object.values(country.languages);
+    return (
+      <div>
+        <h1>{country.name.common}</h1>
+
+        <p>Capital {country.capital}</p>
+        <p>Area {country.area}</p>
+
+        <h3>Languages</h3>
+        <ul>
+          {languages.map((lang) => (
+            <li key={lang}>{lang}</li>
+          ))}
+        </ul>
+
+        <img src={country.flags.png} alt={`${country.name.common} flag`} />
+      </div>
+    );
+  }
+};
