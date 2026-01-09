a06b474975320bb4208fa901ddcb11069b038ac4
Christian Ogunleye <ogunleyetimilehin15@gmail.com>
2026-01-09 17:39:01 +0100

    exercise 3.20*: Phonebook database, step 8 done

--- part3/phonebook/backend/models/person.js
+++ part3/phonebook/backend/models/person.js
@@ -15,7 +15,16 @@ const personSchema = mongoose.Schema({
     required: true,
     minLength: 3,
   },
-  number: String,
+  number: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d{5,}$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    required: [true, "User phone number required"],
+  },
 });
 
 personSchema.set("toJSON", {
