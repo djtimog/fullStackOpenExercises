bfe8efd00fd582b3e24430042526e5ee94f63ece
Christian Ogunleye <ogunleyetimilehin15@gmail.com>
2026-01-14 18:00:31 +0100

    exercise 4.4: Helper Functions and Unit Tests, step 2 DONE

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, cur) => {
+    sum = sum + cur.likes;
+    return sum;
+  };
+  return blogs.reduce(reducer, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
