f9d918b89f352f97f9753fa8027d7acbe2379caa
Christian Ogunleye <ogunleyetimilehin15@gmail.com>
2026-01-19 01:54:44 +0100

    exercise 4.10: Blog List Tests, step 3: wrote a test for posting blog list to the db

--- part4/bloglist/tests/db_list_helper.test.js
+++ part4/bloglist/tests/db_list_helper.test.js
@@ -2,29 +2,51 @@ const supertest = require("supertest");
 const app = require("../app");
 const { test, after, describe } = require("node:test");
 const mongoose = require("mongoose");
+const assert = require("node:assert");
 
 const api = supertest(app);
 describe("Blog list API tests", () => {
+  let blogs;
+  const newBlog = {
+    title: "New Blog title 00323",
+    author: "New Auth",
+    url: "https://example.com/new-blog-00323",
+    likes: 0,
+  };
+
   test("Get all blog list from db", async () => {
-    await api
+    const response = await api
       .get("/api/blogs")
       .expect(200)
       .expect("Content-Type", /application\/json/);
-    // console.log(response);
+
+    blogs = response.body;
   });
 
   test("Blogs had unique identifier property named id", async () => {
-    await api
+    blogs.forEach((blog) => {
+      if (!blog.id) {
+        throw new Error("Blog missing 'id' property");
+      }
+    });
+  });
+
+  test("Add a new blog to the database", async () => {
+    const postResponse = await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const getResponse = await api
       .get("/api/blogs")
       .expect(200)
-      .expect("Content-Type", /application\/json/)
-      .then((response) => {
-        response.body.forEach((blog) => {
-          if (!blog.id) {
-            throw new Error("Blog missing 'id' property");
-          }
-        });
-      });
+      .expect("Content-Type", /application\/json/);
+
+    const { author, likes, title, url } = postResponse.body;
+
+    assert.strictEqual(getResponse.body.length, blogs.length + 1);
+    assert.deepEqual({ author, likes, title, url }, newBlog);
   });
 });
 
