b6888ab3d9a445e0c8df6daf9ec6aa6dde9d89f4
Christian Ogunleye <ogunleyetimilehin15@gmail.com>
2026-01-24 07:44:29 +0100

    5.7 Blog List Frontend, step 7 exercise done

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [viewDetails, setViewDetails] = useState(false);
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setViewDetails(!viewDetails)}>
+          {viewDetails ? "hide" : "view"}
+        </button>
+      </div>
+      {viewDetails && (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes} <button>likes</button>
+          </div>
+          <div>{blog.user.name}</div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
