2682ff54deb0de74606ab712cd74a1f1ccbb44c9
Christian Ogunleye <ogunleyetimilehin15@gmail.com>
2026-02-20 16:39:07 +0100

    Exercise 6.21 done

--- part6/query-anecdotes/src/requests.js
+++ part6/query-anecdotes/src/requests.js
@@ -1,10 +1,32 @@
 const baseUrl = 'http://localhost:3001/anecdotes'
 
+const getId = () => Math.floor(Math.random() * 10000)
 export const getAnecdotes = async () => {
   const response = await fetch(baseUrl)
 
   if (!response.ok) {
     throw new Error('Failed to fetch anecdote')
+  }
+
+  return await response.json()
+}
+
+export const createAnecdotes = async (content) => {
+  const options = {
+    method: 'POST',
+    header: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      content,
+      id: getId(),
+      votes: 0,
+    }),
+  }
+
+  const response = await fetch(baseUrl, options)
+  if (!response.ok) {
+    throw new Error('Failed to create anecdote')
   }
 
   return await response.json()
